e69a116236c7d7115d77ba5765b64250e137e3d4
Kolmogorov Maxim <kolmm3@gmail.com>
2018-02-06 15:53:05 +0500

    add get random function

--- js/setup.js
+++ js/setup.js
@@ -12,26 +12,33 @@ var WIZARD_FIRSTNAME = ['Иван', 'Хуан Себастьян', 'Мария',
 var WIZARD_LASTNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATCOLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYESCOLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+function getRandomItemFromAnArray(arr) {
+  var number = Math.floor(Math.random() * arr.length);
+
+  return arr[number];
+}
+
 var wizards = [
   {
-    name: WIZARD_FIRSTNAME[Math.floor(Math.random() * WIZARD_FIRSTNAME.length)] + ' ' + WIZARD_LASTNAME[Math.floor(Math.random() * WIZARD_LASTNAME.length)],
-    coatColor: WIZARD_COATCOLOR[Math.floor(Math.random() * WIZARD_COATCOLOR.length)],
-    eyesColor: WIZARD_EYESCOLOR[Math.floor(Math.random() * WIZARD_EYESCOLOR.length)]
+    name: getRandomItemFromAnArray(WIZARD_FIRSTNAME) + ' ' + getRandomItemFromAnArray(WIZARD_LASTNAME),
+    coatColor: getRandomItemFromAnArray(WIZARD_COATCOLOR),
+    eyesColor: getRandomItemFromAnArray(WIZARD_EYESCOLOR)
   },
   {
-    name: WIZARD_FIRSTNAME[Math.floor(Math.random() * WIZARD_FIRSTNAME.length)] + ' ' + WIZARD_LASTNAME[Math.floor(Math.random() * WIZARD_LASTNAME.length)],
-    coatColor: WIZARD_COATCOLOR[Math.floor(Math.random() * WIZARD_COATCOLOR.length)],
-    eyesColor: WIZARD_EYESCOLOR[Math.floor(Math.random() * WIZARD_EYESCOLOR.length)]
+    name: getRandomItemFromAnArray(WIZARD_FIRSTNAME) + ' ' + getRandomItemFromAnArray(WIZARD_LASTNAME),
+    coatColor: getRandomItemFromAnArray(WIZARD_COATCOLOR),
+    eyesColor: getRandomItemFromAnArray(WIZARD_EYESCOLOR)
   },
   {
-    name: WIZARD_FIRSTNAME[Math.floor(Math.random() * WIZARD_FIRSTNAME.length)] + ' ' + WIZARD_LASTNAME[Math.floor(Math.random() * WIZARD_LASTNAME.length)],
-    coatColor: WIZARD_COATCOLOR[Math.floor(Math.random() * WIZARD_COATCOLOR.length)],
-    eyesColor: WIZARD_EYESCOLOR[Math.floor(Math.random() * WIZARD_EYESCOLOR.length)]
+    name: getRandomItemFromAnArray(WIZARD_FIRSTNAME) + ' ' + getRandomItemFromAnArray(WIZARD_LASTNAME),
+    coatColor: getRandomItemFromAnArray(WIZARD_COATCOLOR),
+    eyesColor: getRandomItemFromAnArray(WIZARD_EYESCOLOR)
   },
   {
-    name: WIZARD_FIRSTNAME[Math.floor(Math.random() * WIZARD_FIRSTNAME.length)] + ' ' + WIZARD_LASTNAME[Math.floor(Math.random() * WIZARD_LASTNAME.length)],
-    coatColor: WIZARD_COATCOLOR[Math.floor(Math.random() * WIZARD_COATCOLOR.length)],
-    eyesColor: WIZARD_EYESCOLOR[Math.floor(Math.random() * WIZARD_EYESCOLOR.length)]
+    name: getRandomItemFromAnArray(WIZARD_FIRSTNAME) + ' ' + getRandomItemFromAnArray(WIZARD_LASTNAME),
+    coatColor: getRandomItemFromAnArray(WIZARD_COATCOLOR),
+    eyesColor: getRandomItemFromAnArray(WIZARD_EYESCOLOR)
   }
 ];
 
